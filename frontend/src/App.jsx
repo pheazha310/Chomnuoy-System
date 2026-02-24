35bf9a9680308b960c8ae6b81d1ae319e5d76a50
Sophea Phal <pheazha310@gmail.com>
2026-02-24 20:48:05 +0700

    fix: fix code that error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,3 @@
-import Footer from './components/Footer.jsx';
-import Navbar from './components/Navbar.jsx';
-import CampaignsPage from './components/CampaignsPage.jsx';
-import CampaignDetailPage from './components/CampaignDetailPage.jsx';
-import HowItWorksPage from './components/HowItWorksPage.jsx';
-
-function App() {
-  const pathname = window.location.pathname;
-
-  let pageContent = <CampaignsPage />;
-  const campaignDetailMatch = pathname.match(/^\/campaigns\/([^/]+)$/);
-
-  if (campaignDetailMatch) {
-    pageContent = <CampaignDetailPage campaignId={campaignDetailMatch[1]} />;
-  } else if (pathname === '/how-it-works') {
-    pageContent = <HowItWorksPage />;
-  } else if (pathname === '/campaigns' || pathname === '/') {
-    pageContent = <CampaignsPage />;
-  }
-
-  return (
-    <>
-      <Navbar />
-      {pageContent}
-      <Footer />
-    </>
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import ROUTES from './constants/routes.js';
