9829b113c0e93a676df9987a4abe284996aac921
SEYHA.NY <SEYHA.NY@PNC.LOCAL>
2026-02-25 09:39:34 +0700

    fix: fixed code that error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Organization from './organization.jsx';
-import './organization.css';
+import Organization from './components/pages/organization.jsx';
+import './components/css/organization.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
