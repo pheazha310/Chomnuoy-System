2deb2df2490b9c2a299f9a38eb9d1982053754e8
Sophea Phal <pheazha310@gmail.com>
2026-03-30 15:26:17 +0700

    update feature to dynamic

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,14 @@ import { resolve } from 'path';
 
 export default defineConfig({
   server: {
-    host: 'localhost',
+    host: '0.0.0.0',
     port: 5173,
     strictPort: true,
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+      clientPort: 5173,
+    },
   },
   resolve: {
     alias: { '@': resolve(__dirname, 'src') },
